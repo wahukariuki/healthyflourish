72b7b2870a3082de3e049e5515a6d116f93f2f67
Daryn Ongera <ongeradayn@gmail.com>
2023-11-12 22:41:44 +0300

    2factorAuth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-        '2fa' => \PragmaRX\Google2FALaravel\Middleware::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'googlesfa_secret'
+        'google2fa_secret'
     ];
 
     /**
